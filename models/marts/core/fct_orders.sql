f45413a51cc19f142363f191643c1167cbeb6829
Ben Fischer <bfischer@axs.com>
2020-12-08 03:37:20 +0000

    boom shakalaka

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -10,6 +10,7 @@ select
     orders.order_id
     , orders.customer_id
     , payments.amount
+    , count(*)
 from orders 
 join payments on orders.order_id = payments.order_id 
 where payments.status = 'success'
