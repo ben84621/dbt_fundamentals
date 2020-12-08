8e9423e26c642b56a0ff7317bf40adac06b8eac5
Niranjan Naik <nnaik@axs.com>
2020-12-08 03:44:23 +0000

    Removed count order again

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -10,7 +10,6 @@ select
     orders.order_id
     , orders.customer_id
     , payments.amount
-    , count(*)
 from orders 
 join payments on orders.order_id = payments.order_id 
 where payments.status = 'success'
