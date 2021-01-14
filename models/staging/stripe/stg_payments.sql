a839eadf58395fd9adbd5af905973077073c154e
Ben Fischer <bfischer@axs.com>
2021-01-14 04:39:54 +0000

    getting there

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,6 +2,6 @@ select
     orderid as order_id,
     paymentmethod,
     status,
-    amount/100 as amount,
+    {{ cents_to_dollars('amount') }} as amount,
     created as order_date
 from {{source('stripe','payment')}}
